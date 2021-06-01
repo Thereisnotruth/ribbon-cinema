3766ba09347bdf1ee8aa6c18d81d1e9b37db2942
unknown <thereisnotruth12@gmail.com>
2021-06-02 04:40:30 +0900

    sass setting

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { AppProps } from 'next/app';
-import '../styles/globals.css';
+import '../styles/main.scss';
 
-const App = ({ Component, pageProps } : AppProps) => {
-  <Component {...pageProps} />;
-};
+function App({ Component, pageProps } : AppProps) {
+  return <Component {...pageProps} />;
+}
 
 export default App;
