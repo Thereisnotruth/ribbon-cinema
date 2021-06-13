f7f8a31060b08f38c42bd253d63c5fc9cfd0d522
unknown <thereisnotruth12@gmail.com>
2021-06-14 03:35:43 +0900

    modify footer

--- frontend/component/Footer.tsx
+++ frontend/component/Footer.tsx
@@ -3,7 +3,21 @@ import React from 'react';
 export default function Footer() {
 	return (
   <div className="footer">
-    푸터
+    <div className="footer-content">
+      찾아오시는 길: 서울특별시 동대문구 전농동 서울시립대로 163
+    </div>
+    <div className="footer-content">
+      대표이사: 고태진
+    </div>
+    <div className="footer-content">
+      사업자등록번호: 123-45-6789
+    </div>
+    <div className="footer-content">
+      대표 이메일: Thereisnotruth12@gmail.com
+    </div>
+    <div className="footer-content">
+      고객센터: 02-6490-2440
+    </div>
   </div>
 	);
 }
